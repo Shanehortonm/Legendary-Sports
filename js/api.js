d5d738f668520823dd60335f8d1e4b70319ecdc0
Shane Horton <shanehortonm@gmail.com>
2022-11-06 03:49:11 -0500

    adding bootstrap

--- js/api.js
+++ js/api.js
@@ -17,21 +17,6 @@
 //  })
 
 
-//  fetch('https://api.sportsdata.io/v3/nfl/scores/json/Players/DAL?key=6f9e2dbbd2364930824754d2dcb5228f')
-//   .then((response) => {
-//     if (response.ok) {
-//       return response.json();
-//     } else {
-//       throw new Error("NETWORK RESPONSE ERROR");
-//     }
-//   })
-//   .then(data => {
-//     console.log(data);
-//     displayCocktail(data)
-//   })
-//   .catch((error) => console.error("FETCH ERROR:", error));
-
-
 // function fetchTeamData(){
 //    fetch('https://api.sportsdata.io/v3/nfl/scores/json/NewsByTeam/DAL?key=6f9e2dbbd2364930824754d2dcb5228f')
 //    .then(response => {
