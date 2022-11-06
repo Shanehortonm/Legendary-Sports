bf69f9be36922ceb7df9ceb83faa260c8c913766
Shane Horton <shanehortonm@gmail.com>
2022-11-06 03:39:09 -0500

    successfully completed api pull to display table data

--- js/api.js
+++ js/api.js
@@ -1,17 +1,79 @@
-fetch('https://api.sportsdata.io/v3/nfl/scores/json/NewsByTeam/DAL?key=6f9e2dbbd2364930824754d2dcb5228f')
+// fetch('https://api.sportsdata.io/v3/nfl/scores/json/NewsByTeam/DAL?key=6f9e2dbbd2364930824754d2dcb5228f')
 
-.then((response) => {
-    return response.json()
- })
- .then((data) => {
-    console.log(data)
- })
+// .then((response) => {
+//     return response.json()
+//  })
+//  .then((data) => {
+//     console.log(data)
+//  })
+
+//  fetch('https://api.sportsdata.io/v3/nfl/scores/json/Players/DAL?key=6f9e2dbbd2364930824754d2dcb5228f')
+
+// .then((response) => {
+//     return response.json()
+//  })
+//  .then((data) => {
+//     console.log(data)
+//  })
+
+
+//  fetch('https://api.sportsdata.io/v3/nfl/scores/json/Players/DAL?key=6f9e2dbbd2364930824754d2dcb5228f')
+//   .then((response) => {
+//     if (response.ok) {
+//       return response.json();
+//     } else {
+//       throw new Error("NETWORK RESPONSE ERROR");
+//     }
+//   })
+//   .then(data => {
+//     console.log(data);
+//     displayCocktail(data)
+//   })
+//   .catch((error) => console.error("FETCH ERROR:", error));
+
+
+// function fetchTeamData(){
+//    fetch('https://api.sportsdata.io/v3/nfl/scores/json/NewsByTeam/DAL?key=6f9e2dbbd2364930824754d2dcb5228f')
+//    .then(response => {
+//       if(!response.ok) {
+//          throw Error('ERROR')
+//       }
+//       return response.json();
+//    })
+//    .then(data => {
+//       console.log(data);
+//       document.querySelector('#players').innerHTML = '<h1>hello</h1>'
+//    })
+//    .catch(error => {
+//       console.log(error);
+//    });
+// }
+
+
 
  fetch('https://api.sportsdata.io/v3/nfl/scores/json/Players/DAL?key=6f9e2dbbd2364930824754d2dcb5228f')
-
 .then((response) => {
+   if(!response.ok) {
+      throw Error('ERROR')
+   }
     return response.json()
  })
  .then((data) => {
     console.log(data)
+    let tableData = '';
+    data.map((values) => {
+      tableData += `<tr>
+      <td><img src = '${values.PhotoUrl}'/></td>
+      <td>${values.Name}</td>
+      <td>${values.Number}</td>
+      <td>${values.Position}</td>
+      <td>${values.College}</td>
+    </tr>`
+    })
+    document.getElementById('tableBody').innerHTML = tableData
  })
+ .catch(error => {
+   console.log(error);
+ });
+
+
--- js/main.js
+++ js/main.js
@@ -19,52 +19,57 @@ hamburger.addEventListener('click', () => {
 
 
 
-const pastArray = [
-    {
-        "playerName" : "Troy Aikman",
-        "number" : "8",
-        "position" : "QB",
-        "yearInducted" : "2000"
-    },
-    {
-        "playerName" : "",
-        "number" : "",
-        "position" : "",
-        "yearInducted" : ""
-    },
-    {
-        "playerName" : "",
-        "number" : "",
-        "position" : "",
-        "yearInducted" : ""
-    },
-    {
-        "playerName" : "",
-        "number" : "",
-        "position" : "",
-        "yearInducted" : ""
-    },
-    {
-        "playerName" : "",
-        "number" : "",
-        "position" : "",
-        "yearInducted" : ""
-    }
-]
 
 
-function buildTable(data) {
-    let table = document.getElementById('pastTable')
-    for (let i = 0; i < data.length; i++) {
-        let row = `<tr>
-                    <td>${data[i].playerName}</td>
-                    <td>${data[i].number}</td>
-                    <td>${data[i].position}</td>
-                    <td>${data[i].yearInducted}</td>
-                    </tr>`
 
-        table.innerHTML += row
-    }
-};
 
-buildTable(pastArray);
+// const pastArray = [
+//     {
+//         "playerName" : "Troy Aikman",
+//         "number" : "8",
+//         "position" : "QB",
+//         "yearInducted" : "2000"
+//     },
+//     {
+//         "playerName" : "Troy Aikman",
+//         "number" : "8",
+//         "position" : "QB",
+//         "yearInducted" : "2000"
+//     },
+//     {
+//         "playerName" : "Troy Aikman",
+//         "number" : "8",
+//         "position" : "QB",
+//         "yearInducted" : "2000"
+//     },
+//     {
+//         "playerName" : "Troy Aikman",
+//         "number" : "8",
+//         "position" : "QB",
+//         "yearInducted" : "2000"
+//     },
+//     {
+//         "playerName" : "Troy Aikman",
+//         "number" : "8",
+//         "position" : "QB",
+//         "yearInducted" : "2000"
+//     }
+// ]
+
+
+// function buildTable(data) {
+//     let table = document.getElementById('pastTable')
+//     for (let i = 0; i < data.length; i++) {
+//         let row = `<tr>
+//                     <td>${data[i].playerName}</td>
+//                     <td>${data[i].number}</td>
+//                     <td>${data[i].position}</td>
+//                     <td>${data[i].yearInducted}</td>
+//                     </tr>`
+
+//         table.innerHTML += row
+//     }
+// };
+
+
+// buildTable(pastArray);
