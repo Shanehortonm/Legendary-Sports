4073626346ba020c03df43154f7ae0900d0c5e06
Shane Horton <shanehortonm@gmail.com>
2022-11-09 09:36:30 -0500

    changes to quote section

--- js/main.js
+++ js/main.js
@@ -16,3 +16,27 @@ hamburger.addEventListener('click', () => {
 
     hamburger.classList.toggle('toggle');
 });
+
+
+/*
+==========================
+Quote Function
+==========================
+*/
+
+const quoteArray = ["Today, you have 100% of your life left.",
+                    "A winner never stops trying.",
+                    "A coach is someone who tells you what you don't want to hear, who has you see what you don't want to see, so you can be who you have always known you could be",
+                    "The quality of a man’s life is in direct proportion to his commitment to excellence.",
+                    "Leadership is a matter of having people look at you and gain confidence, seeing how you react. If you’re in control, they’re in control"];
+
+
+function changeQuote() {
+    if
+}
+
+
+
+
+
+// document.getElementById('tomQuote').innerHTML = quoteArray;
