8ab7b1ce148e8e2b7776bd1572f480638ae8d46f
Shane Horton <shanehortonm@gmail.com>
2022-11-11 09:23:51 -0500

    work on quote card

--- js/api.js
+++ js/api.js
@@ -16,9 +16,20 @@
 //    });
 // }
 
+async function fetchTeamRecord() {
+   await fetch('https://api.sportsdata.io/v3/nfl/scores/json/Standings/2022?key=6f9e2dbbd2364930824754d2dcb5228f')
 
-function fetchTeamData(){
- fetch('https://api.sportsdata.io/v3/nfl/scores/json/Players/DAL?key=6f9e2dbbd2364930824754d2dcb5228f')
+.then((response) => {
+    return response.json()
+ })
+ .then((data) => {
+    console.log(data)
+ })};
+
+
+
+async function fetchTeamData(){
+ await fetch('https://api.sportsdata.io/v3/nfl/scores/json/Players/DAL?key=6f9e2dbbd2364930824754d2dcb5228f')
 .then((response) => {
    if(!response.ok) {
       throw Error('ERROR')
@@ -44,5 +55,6 @@ function fetchTeamData(){
  });
 };
 
+fetchTeamRecord();
 fetchTeamData();
 
--- js/main.js
+++ js/main.js
@@ -24,19 +24,26 @@ Quote Function
 ==========================
 */
 
-const quoteArray = ["Today, you have 100% of your life left.",
-                    "A winner never stops trying.",
-                    "A coach is someone who tells you what you don't want to hear, who has you see what you don't want to see, so you can be who you have always known you could be",
-                    "The quality of a man’s life is in direct proportion to his commitment to excellence.",
-                    "Leadership is a matter of having people look at you and gain confidence, seeing how you react. If you’re in control, they’re in control"];
+const quotes = [
+    {"quote1" : "Today, you have 100% of your life left."},
+                    
+    {"quote2" : "A winner never stops trying."},
+                    
+    {"quote3" : "A coach is someone who tells you what you don't want to hear, who has you see what you don't want to see, so you can be who you have always known you could be"},
+                    
+    {"quote4" : "The quality of a man’s life is in direct proportion to his commitment to excellence."},
+                    
+    {"quote5" : "Leadership is a matter of having people look at you and gain confidence, seeing how you react. If you’re in control, they’re in control"}
+];
+
+const item = quotes[Math.floor(Math.random() * quotes.length)];
+
+function changeQuote() {
+    document.getElementById('tomQuote').innerHTML = item.quote1;
+};
+
+document.getElementById('change-Quote').onclick = function(){
+    changeQuote()
+};
 
 
-// function changeQuote() {
-//     if
-// }
-
-
-
-
-
-// document.getElementById('tomQuote').innerHTML = quoteArray;
