d4a9c52a1f813beff198a33b61089c3181370190
Shanehortonm <shanehortonm@gmail.com>
2022-11-18 10:10:25 -0500

    food for thought fixed

--- js/main.js
+++ js/main.js
@@ -25,24 +25,22 @@ Quote Function
 */
 
 const quotes = [
-    {"quote1" : "Today, you have 100% of your life left."},
+    {"quote" : "Today, you have 100% of your life left."},
                     
-    {"quote2" : "A winner never stops trying."},
+    {"quote" : "A winner never stops trying."},
                     
-    {"quote3" : "A coach is someone who tells you what you don't want to hear, who has you see what you don't want to see, so you can be who you have always known you could be"},
+    {"quote" : "A coach is someone who tells you what you don't want to hear, who has you see what you don't want to see, so you can be who you have always known you could be"},
                     
-    {"quote4" : "The quality of a man’s life is in direct proportion to his commitment to excellence."},
+    {"quote" : "The quality of a man’s life is in direct proportion to his commitment to excellence."},
                     
-    {"quote5" : "Leadership is a matter of having people look at you and gain confidence, seeing how you react. If you’re in control, they’re in control"}
+    {"quote" : "Leadership is a matter of having people look at you and gain confidence, seeing how you react. If you’re in control, they’re in control"}
 ];
 
-const item = quotes[Math.floor(Math.random() * quotes.length)];
 
 function changeQuote() {
-    document.getElementById('tomQuote').innerHTML = item.quote1;
+    const item = quotes[Math.floor(Math.random() * quotes.length)];
+    document.getElementById('tomQuote').innerHTML = item.quote;
 };
-
-
 
 /*
 ==========================
@@ -63,3 +61,21 @@ function toggleActive(e) {
 
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+
+/*
+==========================
+Mute Function
+==========================
+*/
+
+function toggleMute() {
+    const video = document.getElementById("heroVideo");
+    
+    if(video.muted){
+        video.muted = false;
+    } else {
+        video.muted = true;
+    }
+    
+    }
